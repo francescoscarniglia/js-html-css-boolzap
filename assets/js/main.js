638670c2299fd19731db9ec948b941f7bc8486ec
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-04-19 22:05:18 +0200

    Restart from display single conversation
    to use ul > li

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,19 @@ $(document).ready(function(){
 
   console.log('jQuery ok');
 
-  var newMessage = $('.writing-message .flex-grow-1 .input-message');
-  
+  var conversation = $('.app .content-conversation .main-conversation .your-reply-message');
+  var newMessage = $('.input-message');
+
+  // aggiunta di un nuovo elemento list-item
+    newMessage.keyup(function(e){
+      if(e.which == 13 || e.keyCode == 13 ) {
+        var newText = newMessage.val().trim();
+        console.log(newText);
+        var newReplyMessage = $('.template .reply-message').clone();
+        newReplyMessage.prepend(newText);
+        conversation.append(newReplyMessage);
+        newMessage.val('');
+      }
+    });
 
 });//ready

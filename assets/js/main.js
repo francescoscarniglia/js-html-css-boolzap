1a94a6813e2a0493526712484a659998300eb506
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-04-19 23:27:30 +0200

    Send message complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,19 +2,19 @@ $(document).ready(function(){
 
   console.log('jQuery ok');
 
-  var conversation = $('.app .content-conversation .main-conversation .your-reply-message');
-  var newMessage = $('.input-message');
+  var conversation = $('.conversation .message-list');
 
   // aggiunta di un nuovo elemento list-item
-    newMessage.keyup(function(e){
-      if(e.which == 13 || e.keyCode == 13 ) {
-        var newText = newMessage.val().trim();
-        console.log(newText);
-        var newReplyMessage = $('.template .reply-message').clone();
-        newReplyMessage.prepend(newText);
-        conversation.append(newReplyMessage);
-        newMessage.val('');
+  $('.input-message').keyup(function(e) {
+      if(e.which === 13 || e.keyCode === 13) {
+          var text = $(this).val().trim();
+          if(text !== '') {
+              var template = $('.template .your-message').clone();
+              var newMessage = $('.input-message').val().trim();
+              conversation.append(template.append(newMessage));
+              $('.input-message').val('');
+          }
       }
-    });
+  });
 
 });//ready

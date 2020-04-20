c5aec1050bf2e8c04b6619d043592d66bb39579c
francesco scarniglia <francescoscarniglia@gmail.com>
2020-04-21 00:22:58 +0200

    continue to work
    my pc needs assistance. I had to move to my old windows 7. from 10 pm now I was able to start working again. I try to end the exercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
 
-  console.log('jQuery ok');
-
+  var messageInput = $('.input-message');
+  var sendIcon = $('.record-message i')
   var conversation = $('.conversation .message-list');
 
+  // Show send message icon on message input focus
+
+  messageInput.on('focus blur', function() {
+      sendIcon.toggleClass('fas fa-microphone fas fa-paper-plane');
+  });
+
+  sendIcon.click(function(){
+    var text = $('.input-message').val().trim();
+    if(text !== '') {
+        var template = $('.template .your-message').clone();
+        var templateTime = $('.template .your-message .absolute-time').clone();
+        var newMessage = $('.input-message').val().trim();
+        conversation.append(template.append(newMessage));
+        $('.input-message').val('');
+    }
+  });
 
 
   // aggiunta di un nuovo elemento list-item
@@ -12,6 +28,7 @@ $(document).ready(function(){
           var text = $(this).val().trim();
           if(text !== '') {
               var template = $('.template .your-message').clone();
+              var templateTime = $('.template .your-message .absolute-time').clone();
               var newMessage = $('.input-message').val().trim();
               conversation.append(template.append(newMessage));
               $('.input-message').val('');
